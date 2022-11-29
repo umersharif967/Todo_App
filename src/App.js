8ffc8e05d9a939726bcac245ea613f7288bf5bbd
bitsclansolutions <bitsclansolutions@gmail.com>
2022-11-29 18:51:47 +0500

    done

--- src/App.js
+++ src/App.js
@@ -12,9 +12,8 @@ function App() {
     body: "",
   });
   const [todo, setTodo] = useState([]);
-  const [indexxx, setIndexxx] = useState('');
+  const [indexxx, setIndexxx] = useState("");
   const [change, setChange] = useState(false);
-
 
   const handleChange = (e) => {
     let name = e.target.name;
@@ -30,24 +29,24 @@ function App() {
       setText({ title: "", body: "" });
     }
   };
-  const editText=()=>{
-    const edits = [...todo]
-    edits.splice(indexxx,1,text);
+  const editText = () => {
+    const edits = [...todo];
+    edits.splice(indexxx, 1, text);
     setTodo(edits);
     setText({ title: "", body: "" });
     setChange(false);
-  }
+  };
   const deleteOnClick = (index) => {
     const deleted = [...todo];
-    deleted.splice(index,1);
+    deleted.splice(index, 1);
     setTodo(deleted);
   };
-  const editOnClick = (index)=>{
+  const editOnClick = (index) => {
     const edited = todo.at(index);
     setText(edited);
     setIndexxx(index);
     setChange(true);
-  }
+  };
   return (
     <>
       <h1 className="text-center text-danger">Updated ToDo</h1>
@@ -71,24 +70,25 @@ function App() {
           onChange={handleChange}
         />
         <div>
-          {change ? (<Button
-            variant="outline-secondary text-light btn-dark mt-2 text-capitalize"
-            id="button-addon2"
-            onClick={editText}
-          >
-            Edit
-          </Button>)
-           : (<Button
-           variant="outline-secondary text-light btn-dark mt-2 text-capitalize"
-           id="button-addon2"
-           onClick={addText}
-         >
-           save
-         </Button> )
-          
-          }
+          {change ? (
+            <Button
+              variant="outline-secondary text-light btn-dark mt-2 text-capitalize"
+              id="button-addon2"
+              onClick={editText}
+            >
+              Edit
+            </Button>
+          ) : (
+            <Button
+              variant="outline-secondary text-light btn-dark mt-2 text-capitalize"
+              id="button-addon2"
+              onClick={addText}
+            >
+              save
+            </Button>
+          )}
         </div>
-        {
+        {todo.length > 0 &&(
           <table className="table">
             <thead>
               <tr>
@@ -104,29 +104,29 @@ function App() {
               {todo.map((elem, index) => (
                 <tr key={index}>
                   <th>{index + 1}</th>
-                  <td>
-                    {elem.title}
-                  </td>
+                  <td>{elem.title}</td>
                   <td>{elem.body}</td>
-                  <td><button
+                  <td>
+                    <button
                       onClick={() => editOnClick(index)}
                       className="border-0"
                     >
                       <i className="bi bi-pencil-square ms-3"></i>
                     </button>
-                    </td>
-                    <td><button
+                  </td>
+                  <td>
+                    <button
                       onClick={() => deleteOnClick(index)}
                       className="border-0"
                     >
                       <i className="bi bi-x-square"></i>
                     </button>
-                    </td>
+                  </td>
                 </tr>
               ))}
             </tbody>
           </table>
-        }
+        )}
       </div>
     </>
   );
