455c6147e5b95db69401e4e916ae094eac9b5975
wasifimtiaz72 <wasifimtiaz72@gmail.com>
2023-01-05 19:30:39 +0500

    eslint-formatter

--- src/components/Inputfield.js
+++ src/components/Inputfield.js
@@ -3,39 +3,39 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 const Inputfield = (props) => {
-  console.log('dsd');
-  return (
-    <>
-      <div className="container w-25 m-auto mt-5" style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-        <Form.Control
-          placeholder="Enter Title here..."
-          aria-label="Recipient's username"
-          aria-describedby="basic-addon2" zxccv
-          name="title"
-          value={props.text.title}
-          onChange={props.handleChange}
-        />
-        <div className="ms-2">
-          {props.change ? (
-            <Button
-              variant="outline-secondary text-light btn-dark text-capitalize"
-              id="button-addon2"
-              onClick={props.editText}>
+	console.log('dsd', { props });
+	return (
+		<>
+			<div className="container w-25 m-auto mt-5" style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+				<Form.Control
+					placeholder="Enter Title here..."
+					aria-label="Recipient's username"
+					aria-describedby="basic-addon2" zxccv
+					name="title"
+					value={props.text.title}
+					onChange={props.handleChange}
+				/>
+				<div className="ms-2">
+					{props.change ? (
+						<Button
+							variant="outline-secondary text-light btn-dark text-capitalize"
+							id="button-addon2"
+							onClick={props.editText}>
               Edit
-            </Button>
-          ) : (
-            <Button
-              variant="outline-secondary text-light btn-dark text-capitalize"
-              id="button-addon2"
-              onClick={props.addText}
-            >
+						</Button>
+					) : (
+						<Button
+							variant="outline-secondary text-light btn-dark text-capitalize"
+							id="button-addon2"
+							onClick={props.addText}
+						>
               save
-            </Button>
-          )}
-        </div>
-      </div>
-    </>
-  );
+						</Button>
+					)}
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Inputfield;
