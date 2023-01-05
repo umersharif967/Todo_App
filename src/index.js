01a745fdc673bd4c124b9a3e4e00594a591917b3
wasifimtiaz72 <wasifimtiaz72@gmail.com>
2023-01-05 19:45:09 +0500

    eslint setup

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import  MainPage  from './pages/MainPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <MainPage />
-  </React.StrictMode>
+	<React.StrictMode>
+		<MainPage />
+	</React.StrictMode>
 );
