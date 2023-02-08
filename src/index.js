32773616bf825479c35d64413e6e28ce77174771
bitsclansolutions <bitsclansolutions@gmail.com>
2023-02-08 19:45:00 +0500

    Redux-Simple

--- src/index.js
+++ src/index.js
@@ -8,8 +8,10 @@ import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
+
 		<Provider store={store}>
 			<MainPage />
 		</Provider>
+
 	</React.StrictMode>
 );
